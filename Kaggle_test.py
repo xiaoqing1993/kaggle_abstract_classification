43875af1d6faa23b488019a9e64641570da9f2fe
Chris <xiaoqing1993@gmail.com>
2016-10-13 22:13:46 -0400

    Add files via upload

--- Kaggle_test.py
+++ Kaggle_test.py
@@ -9,27 +9,28 @@ Created on Wed Oct 05 22:41:01 2016
 #import string
 #import numpy as np
 import pandas as pd
-#from sklearn.naive_bayes import BernoulliNB
-#from sklearn.naive_bayes import MultinomialNB
-from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import MultinomialNB
+
+
 #from Multinomial import MultiNaiveBayes
 #from Multinomial import BernouNaiveBayes
-from feature_extraction import FeatureGenerator
+from feature_stemmer import FeatureGenerator
 
 
-F_gnr = FeatureGenerator(max_features=30000)
+F_gnr = FeatureGenerator(binary=False, max_features=70000)
 X_train, X_test = F_gnr.F_extraction_Kaggle()
 y_train = F_gnr.read_y()
 
 print 'training ...'
-clf_lr = LogisticRegression()
-clf_lr.fit(X_train,y_train)
+
+clf_nbM = MultinomialNB()
+clf_nbM.fit(X_train, y_train)
 
 print 'predicting...'
-y_pre = clf_lr.predict(X_test)
+y_pre = clf_nbM.predict(X_test)
 
 print 'saving result...'
-output_name = 'test_y_1000.csv'
+output_name = 'test_y_70k_stemmer.csv'
 test = pd.DataFrame(columns=['id','category'])
 test.id = range(len(y_pre))
 test.category = y_pre
